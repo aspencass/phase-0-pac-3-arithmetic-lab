bb2fcb6d9ff07d5f48819a3ec778df14e7731d98
Cassidy Wagner <caspenw1@gmail.com>
2022-07-25 10:48:35 -0600

    commit tree fd6b187edeaa6bc8de8ba9980d941dd78911a503

---
+++ index.js
@@ -0,0 +1,43 @@
+/* You'll write a total of eight functions. Use the results of 
+running learn test to guide you towards the right function names and functionality.
+*/
+
+function add() {};
+
+function subtract() {};
+
+function multiply() {};
+
+function divide() {};
+
+function add(a, b) {
+    return a + b
+};
+
+function subtract(a, b) {
+    return a - b 
+};
+
+function multiply(a, b) {
+    return a * b
+};
+
+function divide(a, b) {
+    return a / b
+};
+
+function increment(n) { 
+    return n += 1
+};
+
+function decrement(n) {
+    return n -= 1
+};
+
+function makeInt(n = 100) {
+   return parseInt(n, 10)
+};
+
+function preserveDecimal(string = "80.123999") {
+    return parseFloat(string)
+};
